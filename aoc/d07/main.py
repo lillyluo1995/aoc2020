c2c087f9b8b451b67b7b784d6961eaa8ec0d72f4
Lilly Luo <lillyluo1995@gmail.com>
2020-12-07 17:47:10 -0500

    second commit

---
+++ aoc/d07/main.py
@@ -0,0 +1,132 @@
+from typing import IO
+import re
+from collections import deque
+
+
+def parse_line(line):
+    '''given a line, split it into the important stuff
+    i assume the order will always be x x bags contains num x x....'''
+    bagofwords = re.split(r"[\s.,]", line)
+    # there must be a better way to do the below via regex...
+    bagofwords = [x for x in bagofwords if len(x) > 0]
+    return bagofwords
+
+
+def words_to_node(words):
+    '''given an array of words, turn it into a node....
+    assume that it follows the rule:
+    x x bags contain # x x bags # x x  bags ....
+    0 1 2    3       4 5 6 7    8 9 10 11'''
+    parent_color = ' '.join(words[0:2])
+    children_phrase = words[4:]
+
+    if len(children_phrase) == 3:  # no other bags = 3, 1 x x bag = 4
+        return parent_color, []
+    num_children = int(len(children_phrase)/4)
+    children = [' '.join(children_phrase[4*i+1:4*i+3])
+                for i in range(num_children)]
+    return parent_color, children
+
+
+def parse_1(input_file):
+    '''parse in the file and return a mapping with all the stuff'''
+    mapping = {}
+    for line in input_file.readlines():
+        phrase = parse_line(line)
+        parent_color, children = words_to_node(phrase)
+        for child in children:
+            if child in mapping:
+                if parent_color not in mapping[child]:
+                    mapping[child].append(parent_color)
+            else:
+                mapping[child] = [parent_color]
+    return mapping
+
+
+def bfs(mapping, root):
+    '''count number of children in tree starting at root'''
+    queue_ = deque()
+    seen = set()
+
+    queue_.append(root)
+    num_children = 0
+    while queue_:
+        head = queue_.popleft()
+
+        if head in mapping:
+            for parent in mapping[head]:
+                if parent in seen:
+                    continue
+                if parent not in queue_:
+                    queue_.append(parent)
+                    num_children += 1
+        seen.add(head)
+    return num_children
+
+
+def words_to_node_2(words):
+    '''given an array of words, turn it into a node....
+    assume that it follows the rule:
+    x x bags contain # x x bags # x x  bags ....
+    0 1 2    3       4 5 6 7    8 9 10 11'''
+    parent_color = ' '.join(words[0:2])
+    children_phrase = words[4:]
+
+    if len(children_phrase) == 3:  # no other bags = 3, 1 x x bag = 4
+        return parent_color, []
+    num_children = int(len(children_phrase)/4)
+    children = [(' '.join(children_phrase[4*i+1:4*i+3]),
+                 int(children_phrase[4*i])) for i in range(num_children)]
+    return parent_color, children
+
+
+def parse_2(input_file):
+    '''parse to find real children of shiny gold bag...
+    but need to record the #'''
+    mapping = {}
+    for line in input_file.readlines():
+        phrase = parse_line(line)
+        parent_color, children = words_to_node_2(phrase)
+        mapping[parent_color] = children
+    return mapping
+
+
+def p_1(input_file: IO,
+        debug=False):  # pylint: disable=unused-argument
+    '''given a set of rules, how many bags can i use to carry
+    my shiny gold bag? '''
+    mapping = parse_1(input_file)
+
+    # now this is the root of my tree and i'm going to see how
+    # many children it has
+    target = 'shiny gold'
+    return bfs(mapping, target)
+
+
+def dfs(mapping, root):
+    '''dfs stuff'''
+    queue_ = deque()
+    queue_.append(root)
+
+    num_bags = {}
+    num_bags[root] = 0
+    while queue_:
+        head = queue_.pop()  # head is the color
+        children = mapping[head]
+        if not children:
+            num_bags[head] = 0
+        else:
+            for child in children:
+                num_bags[head] += child[1] +\
+                    dfs(mapping, child[0])*child[1]
+    return num_bags[root]
+
+
+def p_2(input_file: IO,
+        debug=False):  # pylint: disable=unused-argument
+    '''how many bags will my shiny gold bag be req'd to hold?'''
+    mapping = parse_2(input_file)
+
+    # now this is the root of my tree and i'm going to see how
+    # many children it has
+    return dfs(mapping, 'shiny gold')
