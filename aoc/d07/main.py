a661a9f9526fd9af6d838a561f5ed6043e1ba0d1
Lilly Luo <lillyluo1995@gmail.com>
2020-12-08 20:54:36 -0500

    clean d07

--- aoc/d07/main.py
+++ aoc/d07/main.py
@@ -12,37 +12,6 @@ def parse_line(line):
     return bagofwords
 
 
-def words_to_node(words):
-    '''given an array of words, turn it into a node....
-    assume that it follows the rule:
-    x x bags contain # x x bags # x x  bags ....
-    0 1 2    3       4 5 6 7    8 9 10 11'''
-    parent_color = ' '.join(words[0:2])
-    children_phrase = words[4:]
-
-    if len(children_phrase) == 3:  # no other bags = 3, 1 x x bag = 4
-        return parent_color, []
-    num_children = int(len(children_phrase)/4)
-    children = [' '.join(children_phrase[4*i+1:4*i+3])
-                for i in range(num_children)]
-    return parent_color, children
-
-
-def parse_1(input_file):
-    '''parse in the file and return a mapping with all the stuff'''
-    mapping = {}
-    for line in input_file.readlines():
-        phrase = parse_line(line)
-        parent_color, children = words_to_node(phrase)
-        for child in children:
-            if child in mapping:
-                if parent_color not in mapping[child]:
-                    mapping[child].append(parent_color)
-            else:
-                mapping[child] = [parent_color]
-    return mapping
-
-
 def bfs(mapping, root):
     '''count number of children in tree starting at root'''
     queue_ = deque()
@@ -63,48 +32,8 @@ def bfs(mapping, root):
         seen.add(head)
     return num_children
 
-
-def words_to_node_2(words):
-    '''given an array of words, turn it into a node....
-    assume that it follows the rule:
-    x x bags contain # x x bags # x x  bags ....
-    0 1 2    3       4 5 6 7    8 9 10 11'''
-    parent_color = ' '.join(words[0:2])
-    children_phrase = words[4:]
-
-    if len(children_phrase) == 3:  # no other bags = 3, 1 x x bag = 4
-        return parent_color, []
-    num_children = int(len(children_phrase)/4)
-    children = [(' '.join(children_phrase[4*i+1:4*i+3]),
-                 int(children_phrase[4*i])) for i in range(num_children)]
-    return parent_color, children
-
-
-def parse_2(input_file):
-    '''parse to find real children of shiny gold bag...
-    but need to record the #'''
-    mapping = {}
-    for line in input_file.readlines():
-        phrase = parse_line(line)
-        parent_color, children = words_to_node_2(phrase)
-        mapping[parent_color] = children
-    return mapping
-
-
-def p_1(input_file: IO,
-        debug=False):  # pylint: disable=unused-argument
-    '''given a set of rules, how many bags can i use to carry
-    my shiny gold bag? '''
-    mapping = parse_1(input_file)
-
-    # now this is the root of my tree and i'm going to see how
-    # many children it has
-    target = 'shiny gold'
-    return bfs(mapping, target)
-
-
 def dfs(mapping, root):
-    '''dfs stuff'''
+    '''dfs to find number of children but need to scale by multiple'''
     queue_ = deque()
     queue_.append(root)
 
@@ -121,11 +50,58 @@ def dfs(mapping, root):
                     dfs(mapping, child[0])*child[1]
     return num_bags[root]
 
+def words_to_node(words):
+    '''given an array of words, turn it into a node....
+    assume that it follows the rule:
+    x x bags contain # x x bags # x x  bags ....
+    0 1 2    3       4 5 6 7    8 9 10 11'''
+    parent_color = ' '.join(words[0:2])
+    children_phrase = words[4:]
+
+    if len(children_phrase) == 3:  # no other bags = 3, 1 x x bag = 4
+        return parent_color, []
+    num_children = int(len(children_phrase)/4)
+    children = [(' '.join(children_phrase[4*i+1:4*i+3]),
+                int(children_phrase[4*i])) for i in range(num_children)]
+    return parent_color, children
+
+def p_1(input_file: IO,
+        debug=False):  # pylint: disable=unused-argument
+    '''given a set of rules, how many bags can i use to carry
+    my shiny gold bag? '''
+
+    def parse(input_file):
+        '''returns map of child -> parent'''
+        mapping = {}
+        for line in input_file.readlines():
+            phrase = parse_line(line)
+            parent_color, children = words_to_node(phrase)
+            for child, _ in children:
+                if child in mapping:
+                    if parent_color not in mapping[child]:
+                        mapping[child].append(parent_color)
+                else:
+                    mapping[child] = [parent_color]
+        return mapping
+    mapping = parse(input_file)
+
+    # now this is the root of my tree and i'm going to see how
+    # many children it has
+    return bfs(mapping, 'shiny gold')
 
 def p_2(input_file: IO,
         debug=False):  # pylint: disable=unused-argument
     '''how many bags will my shiny gold bag be req'd to hold?'''
-    mapping = parse_2(input_file)
+
+    def parse(input_file):
+        '''returns map of parent -> child'''
+        mapping = {}
+        for line in input_file.readlines():
+            phrase = parse_line(line)
+            parent_color, children = words_to_node(phrase)
+            mapping[parent_color] = children
+        return mapping
+    mapping = parse(input_file)
 
     # now this is the root of my tree and i'm going to see how
     # many children it has
