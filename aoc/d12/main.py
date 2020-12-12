b2c8e50d2cb8192ac25cb3f5702452762b32196f
Lilly Luo <lillyluo1995@gmail.com>
2020-12-12 16:24:07 -0500

    Day 12: clean code

--- aoc/d12/main.py
+++ aoc/d12/main.py
@@ -2,26 +2,40 @@ from typing import IO
 import re
 import math
 
+MAPPING = {
+    'N': (0, 1),
+    'S': (0, -1),
+    'E': (1, 0),
+    'W': (-1, 0),
+    'L': 1,  # clockwise
+    'R': -1  # counterclockwise
+}
+
+
 def read_file(input_file):
     '''read the input file of instructions'''
     regex_instr = re.compile('([A-Z])([0-9]+)')
-    directions = [list(regex_instr.match(x).groups()) for x in input_file.readlines()]
+    directions = [list(regex_instr.match(x).groups())
+                  for x in input_file.readlines()]
 
-    #need to use yield as here to be more efficient...
+    # need to use yield as here to be more efficient...
     directions = [[x[0], int(x[1])] for x in directions]
     return directions
+
 
 def add_tuple(tuple_1, tuple_2):
     '''add tuple_1 and tuple_2'''
     return tuple(map(sum, zip(tuple_1, tuple_2)))
 
+
 def mult_tuple(tuple_, scalar_):
     '''multiple tuple_ by scalar_'''
-    return tuple([int(round(scalar_*x,0)) for x in tuple_])
+    return tuple([int(round(scalar_*x, 0)) for x in tuple_])
+
 
 def get_current_angle(start_dir):
     '''get the angle based on the start/end coord...this is basically only 90 angles'''
-    if start_dir == (0,0):
+    if start_dir == (0, 0):
         current_angle = 0
     elif start_dir[0] == 0:
         if start_dir[1] > 0:
@@ -39,6 +53,7 @@ def get_current_angle(start_dir):
             current_angle = math.pi+current_angle
     return current_angle
 
+
 def interpret_dir(start_coord, start_dir, direction):
     '''given the starting coordinate/direction and an instruction, return the
     next location of the ship'''
@@ -48,84 +63,63 @@ def interpret_dir(start_coord, start_dir, direction):
 
     if dir_ == 'F':
         end_coord = add_tuple(start_coord, mult_tuple(start_dir, num_))
-    elif dir_ == 'N':
-        end_coord = add_tuple(start_coord, mult_tuple((0,1), num_))
-    elif dir_ == 'S':
-        end_coord = add_tuple(start_coord, mult_tuple((0,-1), num_))
-    elif dir_ == 'E':
-        end_coord = add_tuple(start_coord, mult_tuple((1, 0), num_))
-    elif dir_ == 'W':
-        end_coord = add_tuple(start_coord, mult_tuple((-1,0), num_))
-    elif dir_ == 'L':
+    elif dir_ in ['N', 'S', 'E', 'W']:
+        end_coord = add_tuple(start_coord, mult_tuple(MAPPING[dir_], num_))
+    elif dir_ in ['L', 'R']:
         num_rad = num_*math.pi/180
-        end_dir = (int(math.cos(current_angle+num_rad)), \
-            int(math.sin(current_angle+num_rad)))
-        end_coord = start_coord
-    elif dir_ == 'R':
-        num_rad = num_*math.pi/180
-        end_dir = (int(math.cos(current_angle-num_rad)), \
-            int(math.sin(current_angle-num_rad)))
+        end_dir = (int(round(math.cos(current_angle+MAPPING[dir_]*num_rad),0)),
+                   int(round(math.sin(current_angle+MAPPING[dir_]*num_rad),0)))
         end_coord = start_coord
     return end_coord, end_dir
+
 
 def interpret_dir_waypoint(coord_, waypoint, direction):
     '''given the starting coordinate/direction and an instruction, return the
     next location of the ship'''
     dir_, num_ = direction
 
-    #moves ship
+    # moves ship
     if dir_ == 'F':
         coord_ = add_tuple(coord_, mult_tuple(waypoint, num_))
-    #moves relative direction of waypoint relative to ship
-    elif dir_ == 'N':
-        waypoint = add_tuple(waypoint, mult_tuple((0,1), num_))
-    elif dir_ == 'S':
-        waypoint = add_tuple(waypoint, mult_tuple((0,-1), num_))
-    elif dir_ == 'E':
-        waypoint = add_tuple(waypoint, mult_tuple((1, 0), num_))
-    elif dir_ == 'W':
-        waypoint = add_tuple(waypoint, mult_tuple((-1,0), num_))
-    #moving the waypoint around the ship, ship in meme place
-    elif dir_ == 'L':
+    # moves relative direction of waypoint relative to ship
+    elif dir_ in ['N', 'S', 'E', 'W']:
+        waypoint = add_tuple(waypoint, mult_tuple(MAPPING[dir_], num_))
+    # moving the waypoint around the ship, ship in meme place
+    elif dir_ in ['L', 'R']:
         num_rad = num_*math.pi/180
         waypoint_ship_angle = get_current_angle((waypoint[0], waypoint[1]))
-        waypoint_ship_dist = math.sqrt(math.pow(waypoint[0],2)+\
-            math.pow(waypoint[1],2))
-        new_waypoint_ship_vector = (math.cos(waypoint_ship_angle+num_rad), \
-            math.sin(waypoint_ship_angle+num_rad))
-        waypoint = mult_tuple(new_waypoint_ship_vector, waypoint_ship_dist)
-    elif dir_ == 'R':
-        num_rad = num_*math.pi/180
-        waypoint_ship_angle = get_current_angle((waypoint[0], waypoint[1]))
-        waypoint_ship_dist = math.sqrt(math.pow(waypoint[0],2)+\
-            math.pow(waypoint[1],2))
-        new_waypoint_ship_vector = (math.cos(waypoint_ship_angle-num_rad), \
-            math.sin(waypoint_ship_angle-num_rad))
+        waypoint_ship_dist = math.sqrt(math.pow(waypoint[0], 2) +
+                                       math.pow(waypoint[1], 2))
+        new_waypoint_ship_vector = (math.cos(waypoint_ship_angle+MAPPING[dir_]*num_rad),
+                                    math.sin(waypoint_ship_angle+MAPPING[dir_]*num_rad))
         waypoint = mult_tuple(new_waypoint_ship_vector, waypoint_ship_dist)
     return coord_, waypoint
+
 
 def get_manhattan_distance(coord):
     '''get the manhattan distance of a coordinate'''
     return int(abs(coord[0])+abs(coord[1]))
 
+
 def p_1(input_file: IO,
-        debug=False): # pylint: disable=unused-argument
+        debug=False):  # pylint: disable=unused-argument
     '''what is the manhattan distance of my boat to the starting point?
     #guess 1 - 1753 too high'''
     directions = read_file(input_file)
-    coord_, dir_ = (0,0), (1,0)
+    coord_, dir_ = (0, 0), (1, 0)
     for direction in directions:
         coord_, dir_ = interpret_dir(coord_, dir_, direction)
     return get_manhattan_distance(coord_)
 
+
 def p_2(input_file: IO,
-        debug=False): # pylint: disable=unused-argument
+        debug=False):  # pylint: disable=unused-argument
     '''what is the manhattan distance of my boat using this waypoint?
     #guess 1 - 1514116849 too high
     #guess 2 - 7680 too low'''
     directions = read_file(input_file)
-    coord_, waypoint_coord = (0,0), (10,1)
+    coord_, waypoint_coord = (0, 0), (10, 1)
     for direction in directions:
-        coord_, waypoint_coord = interpret_dir_waypoint(coord_, waypoint_coord, direction)
-        print(direction, coord_, waypoint_coord)
+        coord_, waypoint_coord = interpret_dir_waypoint(
+            coord_, waypoint_coord, direction)
     return get_manhattan_distance(coord_)
