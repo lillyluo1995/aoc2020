f27c1527d7f41d653913809586cfb6366dae2d5a
Lilly Luo <lillyluo1995@gmail.com>
2020-12-20 11:27:09 -0500

    Day 17: Conway Cubes

--- aoc/d17/main.py
+++ aoc/d17/main.py
@@ -1,112 +1,103 @@
 from typing import IO
-import copy
-import numpy as np
+from itertools import product
 
 
-
-def read_file(input_file):
+def parse(input_file, dims):
     '''read the input file'''
-    return np.array([list(x.replace('\n', '')) for x in input_file.readlines()])
+    input_array = [list(x.replace('\n', '')) for x in input_file.readlines()]
+    state = {}
+    for x_idx, row in enumerate(input_array):
+        for y_idx, col in enumerate(row):
+            state_tup = [x_idx, y_idx]
+            for _ in range(dims-len(state_tup)):
+                state_tup.append(0)
+            if col == '#':
+                state[tuple(state_tup)] = 1
+            else:
+                state[tuple(state_tup)] = 0
+    return state
 
 
-def print_state(current_state):
-    '''print out the current state for debugging'''
-    num_layers = len(current_state)
-    deincrement = int((num_layers-1)/2)
-    for idx, layer in enumerate(current_state):
-        print('z={z}'.format(z=idx-deincrement))
-        for row in layer:
+def get_neighbor_tuples(current_coord):
+    '''get the neighbors coords'''
+    dims = len(current_coord)
+    neighbors = [tuple(map(sum, zip(current_coord, step)))
+                 for step in product([-1, 0, 1], repeat=dims)]
+    neighbors = [
+        neighbor for neighbor in neighbors if neighbor != current_coord]
+    return neighbors
+
+
+def add_neighbors(state):
+    '''get all the new points that we might need to parse over'''
+    empty_state = state.copy()
+    for location, _ in state.items():
+        neighbors = get_neighbor_tuples(location)
+        for neighbor in neighbors:
+            if neighbor not in state:
+                empty_state[neighbor] = 0
+    return empty_state
+
+
+def print_state(state):
+    '''print out the current state for debugging...only works for 3 dim'''
+    all_tuples = list(state.keys())
+    min_x, min_y, min_z = [min([tup[i] for tup in all_tuples])
+                           for i in range(3)]
+    max_x, max_y, max_z = [max([tup[i] for tup in all_tuples])
+                           for i in range(3)]
+
+    for z_idx in range(min_z, max_z+1):
+        print('z={z}'.format(z=z_idx))
+        for x_idx in range(min_x, max_x+1):
+            row = []
+            for y_idx in range(min_y, max_y+1):
+                row.append(str(state[(x_idx, y_idx, z_idx)]))
             print(''.join(row))
         print('\n')
 
 
-def initiate_next(current_state):
-    '''instantiate the next one empty'''
-    num_rows, num_cols = len(current_state[0]), len(current_state[0][0])
-    # first we'll add the layers and shift accordingly...
-    new_state = copy.deepcopy(current_state)
-    for idx, layer in enumerate(current_state):
-        # add columns
-        new_col = ['.']*len(layer)
-        temp_new = np.vstack((new_col, layer, new_col))
-        # add rows
-        new_row = np.array(['.']*len(temp_new))[np.newaxis]
-        new_row = np.transpose(new_row)
-        new_state[idx] = np.hstack((new_row, temp_new, new_row))
-
-    # now add the row and shift....
-    new_state.insert(0, np.array([['.']*(num_cols+2)]*(num_rows+2)))
-    new_state.append(np.array([['.']*(num_cols+2)]*(num_rows+2)))
+def iterate(state):
+    '''move from one state to the next'''
+    empty_state = add_neighbors(state)
+    new_state = empty_state.copy()
+    for location, value in empty_state.items():
+        neighbors = get_neighbor_tuples(location)
+        num_active_neighbors = 0
+        for neighbor in neighbors:
+            if neighbor in empty_state:
+                num_active_neighbors += empty_state[neighbor]
+        if value == 1:
+            if num_active_neighbors not in [2, 3]:
+                new_state[location] = 0
+        else:
+            if num_active_neighbors == 3:
+                new_state[location] = 1
     return new_state
 
 
-def check_neighbors(current_state, current_coord):
-    '''count how many active neighbors a cell has'''
-    num_active = 0
-
-    # since we appended one row, col, and layer to the beginning for all
-    for z_chg in [-1, 0, 1]:
-        for x_chg in [-1, 0, 1]:
-            for y_chg in [-1, 0, 1]:
-                if abs(z_chg)+abs(x_chg)+abs(y_chg) == 0:
-                    continue
-                if current_coord[0]+z_chg > len(current_state)-1 \
-                    or current_coord[1]+x_chg > len(current_state[0])-1 \
-                        or current_coord[2]+y_chg > len(current_state[0][0])-1:
-                    continue  # if out of bounds, deffo not active
-
-                neighbor = \
-                    current_state[current_coord[0] +
-                                    z_chg][current_coord[1]+x_chg][current_coord[2]+y_chg]
-                if neighbor == '#':
-                    num_active += 1
-    return num_active
-
-
-def iteration(current_state):
-    '''iterate the current state'''
-    z_start, x_start, y_start = len(current_state), len(current_state[0]), \
-        len(current_state[0][0])
-    empty = initiate_next(current_state)
-    next_empty = copy.deepcopy(empty)
-    for z_c in range(z_start+2):
-        for x_c in range(x_start+2):
-            for y_c in range(y_start+2):
-                current = empty[z_c][x_c][y_c]
-                num_active_neighbors = check_neighbors(empty, [z_c, x_c, y_c])
-                if current == '#':
-                    if num_active_neighbors not in [2, 3]:
-                        next_empty[z_c][x_c][y_c] = '.'
-                else:
-                    if num_active_neighbors == 3:
-                        next_empty[z_c][x_c][y_c] = '#'
-    return next_empty
-
-
-def count_active(current_state):
+def count_active(state):
     '''count number of active states'''
-    num_active = 0
-    for z_coord, _ in enumerate(current_state):
-        for x_coord, _ in enumerate(current_state[0]):
-            for y_coord, _ in enumerate(current_state[0][0]):
-                if current_state[z_coord][x_coord][y_coord] == '#':
-                    num_active += 1
-    return num_active
+    return sum([value for _, value in state.items()])
 
 
 def p_1(input_file: IO,
         debug=False):  # pylint: disable=unused-argument
-    '''preallocate the space....'''
+    '''return number of active states with 3 dimensions'''
     num_iter = 6
-
-    current = [read_file(input_file)]
-    for cyc in range(num_iter):
-        print('CYCLE NUMBER = {cyc}'.format(cyc=cyc+1))
-        current = iteration(current)
+    dims = 3
+    current = parse(input_file, dims)
+    for _ in range(num_iter):
+        current = iterate(current)
     return count_active(current)
 
 
 def p_2(input_file: IO,
         debug=False):  # pylint: disable=unused-argument
-    '''do part 2'''
-    pass
+    '''return number of active states with 3 dimensions'''
+    num_iter = 6
+    dims = 4
+    current = parse(input_file, dims)
+    for _ in range(num_iter):
+        current = iterate(current)
+    return count_active(current)
